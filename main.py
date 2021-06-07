5aa0453f4ffd58fea9e93d328dc01a6ee23e1b52
Josh <joshjjackson91@gmail.com>
2021-06-07 11:16:40 -0500

    working code, with python 3.9

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ def say_hi():
     if name == '':
         print('you did not enter a name!')
     else:
-        print("hi there...")
-        for letter in name:
-            print(letter)
-name = input()
-say_hi(name)
+        print("Hello",name)
+
+name = input("Please Enter Name: ")
+say_hi()
