d365c1166e597aeac642e3e6dd30cf461fef9ecc
Josh <joshjjackson91@gmail.com>
2021-06-07 10:16:07 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+def say_hi():
+    if name == '':
+        print('you did not enter a name!')
+    else
+        print("hi there...")
+        for letter in name:
+            print(letter)
+name = input()
+say_hi(name)
