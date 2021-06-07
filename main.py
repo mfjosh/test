91b9596093bc936d534761e1c5b63112e4787747
Josh <joshjjackson91@gmail.com>
2021-06-07 12:10:32 -0500

    Added Age verification

--- main.py
+++ main.py
@@ -4,6 +4,17 @@ def say_hi():
         print('you did not enter a name!')
     else:
         print("Hello",name)
+def show_age():
+    if age =='':
+        print('You must enter an age!')
+    else:
+        print('So you are',age)
 
+
+
+#Main program sequence
+print("Welcome to the program! Let's get started.")
 name = input("Please Enter Name: ")
 say_hi()
+age = input("What is your age?: ")
+show_age()
