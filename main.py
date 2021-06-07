aa7b518a4f6b881a3daa0db891bd5bc97a86339a
Josh <joshjjackson91@gmail.com>
2021-06-07 12:11:24 -0500

    better category, version 1.1b created

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-#Version 0.1b
+#version 0.1.1b
+#variables
+def introduction():
+    print("Welcome to the program! Let's get started.")
 def say_hi():
     if name == '':
         print('you did not enter a name!')
     else:
         print("Hello",name)
+def show_age():
+    if age =='':
+        print('You must enter an age!')
+    else:
+        print('So you are',age)
 
+
+#Main program sequence
+introduction()
 name = input("Please Enter Name: ")
 say_hi()
+age = input("What is your age?: ")
+show_age()
